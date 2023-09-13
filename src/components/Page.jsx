2d976701edc01e3d493607d57cc2604b1db8e5e9
LucasLunarj <87677530+LucasLunarj@users.noreply.github.com>
2023-09-12 22:45:53 -0300

    Revert "New deploy changes"
    This reverts commit 744cf9a9578898ffcee9fc60185de1304f6b5ffa.

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,10 +1,12 @@
-
+import { useState } from 'react';
 import { useTableContext } from '../contexts/TableContext';
 import { NumberSelect } from './NumberSelect'
 import styles from './Page.module.css'
 export const Page = () => {
     const context = useTableContext();
     const multiplicationTable = ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    console.log(context.tableData.display)
+
 
     return (
         <div className={styles.container}>
